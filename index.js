98b7629c116ae4b026c5727d4812c83e161ce971
saul <saul@.hotmail.com>
2022-03-28 10:14:54 -0500

    conectando a la base datos con mongoose

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hola mundo');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://0.0.0.0:27017/bdbacken')
+        .then(()=>{
+            console.log('se conecto correctamente')
+        })
+        .catch(err=>{
+            console.log(err)
+        })
